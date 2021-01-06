2a4c29f39605e789f2aef6ec548ed5c208f3a6d4
Hiếu Nguyễn <namhieu1102@gmail.com>
2021-01-06 15:43:16 +0700

    Lesson 06: Effect and Sound

---
+++ Assets/Lesson6/Scripts/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public AudioSource AudioSource;
+        public AudioSource SoundSource;
+        public SoundList SoundList;
+
+        public void PlaySound()
+        {
+            SoundSource.Play();
+        }
+        public void ChangeSound(AudioClip music)
+        {
+            if (AudioSource.clip.name == music.name)
+            {
+                return;
+            }
+            AudioSource.Stop();
+            AudioSource.clip = music;
+            AudioSource.Play();
+        }
+        public void PlayAudioClip(string name)
+        {
+            AudioClip clip = SoundList.getAudioClip(name);
+            if (clip == null)
+            {
+                return;
+            }
+            
+            if (AudioSource != null && AudioSource.clip != null)
+            {
+                if(AudioSource.clip.name == clip.name)
+                {
+                    return;
+                }  
+            }
+            AudioSource.Stop();
+            AudioSource.clip = clip;            
+            AudioSource.Play();
+        }
+        public void StopMusic()
+        {
+            AudioSource.clip = null;
+            AudioSource.Stop();
+        }
+        public void TurnOnOffMusic(bool isOn)
+        {
+            AudioSource.mute = !isOn;
+        }
+
+        public void TurnOnOffSound(bool isOn)
+        {
+            SoundSource.mute = !isOn;
+        }
+    }
+}
---
+++ Assets/Lesson6/Scripts/SoundListConstant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audio
+{
+    public class SoundListConstant
+    {
+        public const string LOGIN = "LOGIN";
+        public const string CITY = "CITY";
+        public const string WORLD = "WORLD";
+        public const string BATTLE = "BATTLE";
+        public const string BATTLE_VICTORY = "BATTLE_VICTORY";
+        public const string BATTLE_DEFEAT = "BATTLE_DEFEAT";
+    }
+}
