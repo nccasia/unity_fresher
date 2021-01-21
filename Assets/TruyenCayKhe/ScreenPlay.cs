785583418e600c50d7d01a346c7c14af88f8a5ef
tung.tranhuy <tung.tranhuy@DESKTOP-J2246PJ>
2021-01-22 02:15:12 +0700

    [Film making] more material for "phong bat"

--- Assets/TruyenCayKhe/ScreenPlay.cs
+++ Assets/TruyenCayKhe/ScreenPlay.cs
@@ -38,10 +38,20 @@ public class ScreenPlay : MonoBehaviour
     public GameObject Light_CoEm;
     public GameObject Light_CoChi;
 
+    [Space] [Header("Phong bat")] 
+    public Material phongbat_2Nha;
+
+    public Material phongbat_Island;
+    public Material phongbat_Jail;
+
+    public MeshRenderer PhongBat;
+    
+
     private WaitForEndOfFrame _eof;
     // Start is called before the first frame update
     void Start()
     {
+        cayKhe.SetActive(false);
         StartCoroutine(StartScenePlay_CayKhe());
     }
 
@@ -67,9 +77,15 @@ public class ScreenPlay : MonoBehaviour
         yield return new WaitForSeconds(17);
         nvChiDau.GetComponent<NavMeshAgent>().destination = nvEmDau.position;
         
+        // TODO: change background
+        PhongBat.material = phongbat_2Nha;
+        
         // Di sang em chong`
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(3);
+        cayKhe.SetActive(true);
+        yield return new WaitForSeconds(7);
         Destroy(objQuaKhe);
+        
         nvChiDau.GetComponent<NavMeshAgent>().destination = nvEm.position;
         yield return new WaitForSeconds(2);
         Destroy(objBug);
@@ -84,7 +100,7 @@ public class ScreenPlay : MonoBehaviour
         yield return new WaitForSeconds(1f);
         nvAnh.GetComponent<NavMeshAgent>().destination = nvLuaSu.position;
         yield return new WaitForSeconds(2f);
-        Destroy(objDollars);
+        objDollars.transform.parent = nvLuaSu;
         nvLuaSu.GetComponent<NavMeshAgent>().destination = nvChim.position;
         yield return _eof;
     }
@@ -95,9 +111,23 @@ public class ScreenPlay : MonoBehaviour
         nvAnh.GetComponent<NavMeshAgent>().destination = chairNguoiAnh.position;
         nvChiDau.GetComponent<NavMeshAgent>().destination = chairCoChi.position;
         yield return new WaitForSeconds(2f);
+        
+        // Lighting & shit
         Light_TrungTam.SetActive(false);
         Light_CoChi.SetActive(true);
         food.SetActive(true);
         goldBars.SetActive(false);
+        
+        yield return new WaitForSeconds(14.3f);
+        // Chuyen canh sang co em: Bon' mua` fix bug
+        Light_CoChi.SetActive(false);
+        Light_CoEm.SetActive(true);
+        
+        yield return new WaitForSeconds(13f);
+        // Chim enter with Moonwalk
+        nvChim.GetComponent<NavMeshAgent>().destination = cayKhe.transform.position;
+        
+        // Oi, nha` dau tu thien than
+        yield return new WaitForSeconds(15f);
     }
 }
