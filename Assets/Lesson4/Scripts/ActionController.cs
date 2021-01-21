c49bc45e52497fd7fbe3ad31b05d8677b274aa12
Hiếu Nguyễn <namhieu1102@gmail.com>
2021-01-21 11:02:59 +0700

    lesson4 - frame by frame animation

---
+++ Assets/Lesson4/Scripts/ActionController.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class ActionController : MonoBehaviour
+{
+    public Animator animator;
+    private void Start() {
+        // render("ATTACK_ATK");
+    }
+    public void render(string animation)
+    {
+        Debug.Log("render " + animation);
+        animator.Play(animation);
+    }
+}
