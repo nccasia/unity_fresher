0b4e3cd2c5e9050581b794105e5a1815eaab41a3
tung.tranhuy <tung.tranhuy@DESKTOP-J2246PJ>
2021-03-11 10:25:37 +0700

    [Lesson10] Design pattern (not completed)

---
+++ Assets/Lesson10/Scripts/ScoreTracking.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class ScoreTracking : MonoBehaviour
+{
+    private static ScoreTracking _instance;
+
+    public static ScoreTracking Instance
+    {
+        get => _instance;
+        private set => _instance = value;
+    }
+
+    private int _score;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        _instance = new ScoreTracking();
+    }
+
+    public int GetScore()
+    {
+        return _score;
+    }
+
+    public void IncreaseScoreBy(int value)
+    {
+        if (value <= 0) return;
+        
+        _score += value;
+    }
+}
---
+++ Assets/Lesson10/Scripts/SomeGamePlay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SomeGamePlay : MonoBehaviour
+{
+    public Text _txtScore;
+    public Button _btnAddOne;
+
+    private void Awake()
+    {
+        _btnAddOne?.onClick.AddListener(OnPlayerWin);
+    }
+
+    private void OnPlayerWin()
+    {
+        ScoreTracking.Instance.IncreaseScoreBy(1);
+
+        _txtScore.text = ScoreTracking.Instance.GetScore() + "$";
+    }
+}
