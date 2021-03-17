7fb7cabad2243b895f1ebfccfb19cdc75e27ffb8
Hiếu Nguyễn <namhieu1102@gmail.com>
2021-03-17 15:18:41 +0700

    lesson 13 - assetbundle - local

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,16 @@
+﻿
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundle
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundle(){
+        string assetBundleDirectory = "Assets/StreamingAssets";
+        if(!Directory.Exists(Application.streamingAssetsPath)){
+            Directory.CreateDirectory("assetBundleDirectory");
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
---
+++ Assets/Lesson13/Script/BundledObjectLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class BundledObjectLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string assetName = "BundledSpriteObject";
+    public string bundleName = "testbundle";
+    void Start()
+    {
+        AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
+        if(localAssetBundle == null) {
+            Debug.Log("Failed to load~");
+            return;
+        }
+        GameObject asset = localAssetBundle.LoadAsset<GameObject>(assetName);
+        Instantiate(asset);
+        localAssetBundle.Unload(false);
+    }
+}
