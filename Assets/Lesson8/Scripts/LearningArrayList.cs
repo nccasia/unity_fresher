8e79d854cd3f43b386c0aba2ea4aa34a4449da09
Hiếu Nguyễn <namhieu1102@gmail.com>
2021-05-13 13:23:37 +0700

    lesson8

---
+++ Assets/Lesson8/Scripts/LearningArrayList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Learning
+{
+    public class LearningArrayList : MonoBehaviour
+    {
+        public ArrayList inventory = new ArrayList();
+        public GameObject item;
+
+        private void Start() {
+            inventory.Add(1);
+            inventory.Add("B");
+            inventory.Add("C");
+            inventory.Add(item);
+        }
+    }
+}
